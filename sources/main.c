d15d79dd511191d553f2a19e7f44dc99130a9be5
Juwon Kim <juwkim@c2r1s8.42seoul.kr>
2022-12-23 17:49:44 +0900

    last

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:41:37 by juwkim            #+#    #+#             */
-/*   Updated: 2022/12/23 16:58:43 by juwkim           ###   ########.fr       */
+/*   Updated: 2022/12/23 17:49:31 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	main(int argc, char *argv[])
 	init_player(&game);
 	init_camera(&game);
 	create_monster(&game);
-	mlx_hook(game.window, 17, 1L << 0, close_hook, &game);
 	mlx_hook(game.window, 2, 0, keydown_hook, &game);
-	mlx_key_hook(game.window, keyup_hook, &game);
+	mlx_hook(game.window, 3, 0, keyup_hook, &game);
+	mlx_hook(game.window, 17, 1L << 17, close_hook, &game);
 	mlx_loop_hook(game.mlx, game_loop, &game);
 	mlx_loop(game.mlx);
 	exit(EXIT_SUCCESS);
@@ -33,10 +33,9 @@ int	main(int argc, char *argv[])
 // General loop of the game which will be executed at best every 15ms
 static int	game_loop(t_game *game)
 {
-	long long	now;
+	const long long	now = millitimestamp();
 	long long	diff_millisecs;
 
-	now = millitimestamp();
 	diff_millisecs = now - game->time_stamp;
 	if (diff_millisecs > 15)
 	{

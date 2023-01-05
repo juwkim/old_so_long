234ac484f203aaa28c67b51f51d0a76a0d6c2e9f
Juwon Kim <juwkim@c1r2s3.42seoul.kr>
2023-01-06 06:40:46 +0900

    end map

--- sources/core/dfs.c
+++ sources/core/dfs.c
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 05:50:55 by juwkim            #+#    #+#             */
-/*   Updated: 2023/01/06 06:28:38 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/01/06 06:36:18 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ void	free_visited(char **visited, int x)
 		free(visited[idx++]);
 }
 
-
 int	dfs(char **map, char **visited, int x, int y)
 {
 	const int	dx[4] = {0, 1, 0, -1};
@@ -63,30 +62,14 @@ int	dfs(char **map, char **visited, int x, int y)
 
 int	check_collectable(t_game *game)
 {
-	int		i;
-	int		j;
-	int		count;
-	char	**visited;
+	int				count;
+	char			**visited;
+	const int		i = game->player.position.second / BLOCK_SIZE;
+	const int		j = game->player.position.first / BLOCK_SIZE;
 
-	i = 0;
-	j = 0;
-	count = 0;
 	visited = get_visited_array(game->map_size.second, game->map_size.first);
-	ft_printf("%d %d\n", game->map_size.second, game->map_size.first);
-	while (++i < game->map_size.second - 1)
-	{
-		j = 0;
-		while (++j < game->map_size.first - 1)
-		{
-			ft_printf("%d %d\n", i, j);
-			if (visited[i][j] == 1 || game->map[i][j] == CHAR_WALL)
-				continue ;
-			visited[i][j] = 1;
-			count += dfs(game->map, visited, i, j);
-			if (count == game->count[COLLECTABLE])
-				return (1);
-		}
-	}
+	visited[i][j] = 1;
+	count = dfs(game->map, visited, i, j);
 	free_visited(visited, game->map_size.second);
 	return (count == game->count[COLLECTABLE]);
 }
--- sources/tools/error.c
+++ sources/tools/error.c
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:41:11 by juwkim            #+#    #+#             */
-/*   Updated: 2022/12/28 03:01:41 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/01/06 06:40:15 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void	error(int error_n)
 {
 	const char *const	error_message[4] = \
 	{
-		"Error: You must indicate only one map path !\n", \
-		"Error: Unable to open the map !\n", \
-		"Error: The map is not in a valid format !\n", \
-		"Error: A sprite could not be opened !\n"
+		"Error\nYou must indicate only one map path!\n", \
+		"Error\nUnable to open the map!\n", \
+		"Error\nThe map is not in a valid format!\n", \
+		"Error\nAn image could not be opened!\n"
 	};
 
 	ft_printf(error_message[error_n]);
